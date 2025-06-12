37c6cff047eb7ef21a559862ee02d29d15734329
cynarski <cynarski60@gmail.com>
2025-06-12 15:21:15 +0200

    Fix

--- database/views/Match_statistic.sql
+++ database/views/Match_statistic.sql
@@ -1,30 +1,24 @@
 DROP VIEW IF EXISTS Match_statistics;
 
 CREATE VIEW Match_statistics AS
-SELECT
-	m.id,
-	t1.TeamName AS team1,
-	t2.TeamName AS team2,
-	md.T1_Srv_Ace,
-	md.T1_Srv_Err,
-	md.T2_Srv_Ace,
-	md.T2_Srv_Err,
-
-	md.T1_Rec_pos,
-	md.T1_Rec_perf,
-
-	md.T2_Rec_pos,
-	md.T2_Rec_perf,
-
-	md.T1_att_err,
-	md.T1_att_kill_perc,
-	md.T2_att_err,
-	md.T2_att_kill_perc,
-
-	md.T1_Blk_Sum,
-	md.T2_Blk_Sum
-
-FROM Match_details md
-JOIN Matches m ON md.match_id = m.id
-JOIN Team t1 ON m.team_1 = t1.id
-JOIN Team t2 ON m.team_2 = t2.id;
+SELECT m.id,
+    t1.teamname AS team1,
+    t2.teamname AS team2,
+    md.t1_srv_ace,
+    md.t1_srv_err,
+    md.t2_srv_ace,
+    md.t2_srv_err,
+    md.t1_rec_pos,
+    md.t1_rec_perf,
+    md.t2_rec_pos,
+    md.t2_rec_perf,
+    md.t1_att_err,
+    md.t1_att_kill_perc,
+    md.t2_att_err,
+    md.t2_att_kill_perc,
+    md.t1_blk_sum,
+    md.t2_blk_sum
+FROM (((match_details md
+ JOIN matches m ON ((md.match_id = m.id)))
+ JOIN team t1 ON ((m.team_1 = t1.id)))
+ JOIN team t2 ON ((m.team_2 = t2.id)));
